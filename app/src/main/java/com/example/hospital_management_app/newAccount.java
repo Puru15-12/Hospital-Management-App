3ce6cdcc59ca92cf972ac7e5d5cc4f9bd50f24a2
Piyush20-11 <piyush275202@gmail.com>
2023-04-03 16:19:44 +0530

    Password Textfield added

--- app/src/main/java/com/example/hospital_management_app/newAccount.java
+++ app/src/main/java/com/example/hospital_management_app/newAccount.java
@@ -3,6 +3,7 @@ package com.example.hospital_management_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class newAccount extends AppCompatActivity {
@@ -13,6 +14,9 @@ public class newAccount extends AppCompatActivity {
         setContentView(R.layout.activity_new_account);
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
-
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(newAccount.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Select_city));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
