525bde96dc33d4e4f4aec753af555d280624ff9b
Piyush20-11 <piyush275202@gmail.com>
2023-04-03 15:49:13 +0530

    spinner added

--- app/src/main/java/com/example/hospital_management_app/newAccount.java
+++ app/src/main/java/com/example/hospital_management_app/newAccount.java
@@ -3,6 +3,7 @@ package com.example.hospital_management_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class newAccount extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class newAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+
     }
 }
