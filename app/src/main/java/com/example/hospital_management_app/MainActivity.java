84bb34accf30cb6f3d834165539cbf164f60d5aa
Piyush20-11 <piyush275202@gmail.com>
2023-04-03 14:32:00 +0530

    Intermediate commit

---
+++ app/src/main/java/com/example/hospital_management_app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hospital_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnNext;
+        btnNext=findViewById(R.id.btnNext);
+        Intent iNext;
+        iNext=new Intent(getApplicationContext(), newAccount.class);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(iNext);
+            }
+        });
+    }
+}
