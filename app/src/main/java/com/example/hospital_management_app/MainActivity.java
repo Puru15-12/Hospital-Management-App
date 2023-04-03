cea2c04f354bfb54626caeaaa1fc7dbc6c0e342e
Puru15-12 <singhpuru36@gmail.com>
2023-04-03 16:26:05 +0530

    login backend added

--- app/src/main/java/com/example/hospital_management_app/MainActivity.java
+++ app/src/main/java/com/example/hospital_management_app/MainActivity.java
@@ -8,20 +8,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnNext;
-        btnNext=findViewById(R.id.btnNext);
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
         Intent iNext;
         iNext=new Intent(getApplicationContext(), newAccount.class);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 startActivity(iNext);
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this ,Login_activity.class));
+                finish();
             }
         });
     }
