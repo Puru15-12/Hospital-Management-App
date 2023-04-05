2f9cb7f6718e47057c7214a82903c2429f78c993
Puru15-12 <singhpuru36@gmail.com>
2023-04-05 14:41:33 +0530

    Register_page almost done

--- app/src/main/java/com/example/hospital_management_app/Login_activity.java
+++ app/src/main/java/com/example/hospital_management_app/Login_activity.java
@@ -1,54 +1,53 @@
-//package com.example.hospital_management_app;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.Toast;
-//
-//import com.google.android.gms.tasks.OnSuccessListener;
-//import com.google.firebase.auth.AuthResult;
-//import com.google.firebase.auth.FirebaseAuth;
-//
-//public class Login_activity extends AppCompatActivity {
-//
-//    private EditText phone;
-//    private EditText password;
-//    private Button login;
-//    private FirebaseAuth auth;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-//
-//        phone=findViewById(R.id.editTextTextEmailAddress);
-//        password=findViewById(R.id.editTextNumberPassword);
-//        login=findViewById(R.id.login);
-//        auth=FirebaseAuth.getInstance();
-//
-//        login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String text_phone=phone.getText().toString();
-//                String text_password=password.getText().toString();
-//                loginuser(text_phone,text_password);
-//            }
-//        });
-//    }
-//
-//    private void loginuser(String text_phone, String text_password) {
-//
-//        auth.signInWithEmailAndPassword(text_phone,text_password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//            @Override
-//            public void onSuccess(AuthResult authResult) {
-//                Toast.makeText(Login_activity.this,"Login Succesful",Toast.LENGTH_SHORT).show();
-//                startActivity(new Intent(Login_activity.this,MainActivity.class));
-//                finish();
-//            }
-//        });
-//    }
-//
-//}
+package com.example.hospital_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login_activity extends AppCompatActivity {
+
+    private EditText phone;
+    private EditText password;
+    private Button log;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        phone=findViewById(R.id.editTextTextEmailAddress);
+        password=findViewById(R.id.editTextNumberPassword);
+        log=findViewById(R.id.login);
+        auth=FirebaseAuth.getInstance();
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text_phone=phone.getText().toString();
+                String text_password=password.getText().toString();
+                loginuser(text_phone,text_password);
+            }
+        });
+    }
+
+    private void loginuser(String text_phone, String text_password) {
+
+        auth.signInWithEmailAndPassword(text_phone,text_password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(Login_activity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login_activity.this,MainActivity.class));
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/hospital_management_app/newAccount.java
+++ app/src/main/java/com/example/hospital_management_app/newAccount.java
@@ -1,35 +1,38 @@
 package com.example.hospital_management_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.concurrent.atomic.AtomicStampedReference;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class newAccount extends AppCompatActivity {
-    private EditText first_name;
-    private EditText last_name;
-    private EditText email;
-    private EditText mob_no;
-    private RadioButton patient;
-    private RadioButton ambulance;
-    private RadioButton doctor;
-    private RadioButton male;
-    private RadioButton female;
-    private RadioButton others;
-    private EditText password;
-    private EditText confirm;
+    private EditText first_name,last_name,email,mob_no;
+    private RadioButton patient,ambulance,doctor;
+    private EditText password,confirm;
     private Button register;
-    private TextView no_need;
-    private TextView login;
+    private TextView no_need,login;
+    UserHelperClass member=new UserHelperClass();
+       FirebaseDatabase rootnode;
+       DatabaseReference refrence;
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,15 +51,35 @@ public class newAccount extends AppCompatActivity {
         patient=findViewById(R.id.radioButton);
         ambulance=findViewById(R.id.radioButton2);
         doctor=findViewById(R.id.radioButton3);
-        male=findViewById(R.id.radioButton4);
-        female=findViewById(R.id.radioButton5);
-        others=findViewById(R.id.radioButton6);
         password=findViewById(R.id.editTextNumberPassword);
         confirm=findViewById(R.id.editTextNumberPassword2);
         register=findViewById(R.id.button2);
         no_need=findViewById(R.id.textView6);
         login=findViewById(R.id.textView7);
 
+        rootnode= FirebaseDatabase.getInstance();
+
+        refrence= rootnode.getReference("users");
+
+        no_need.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(newAccount.this,MainActivity.class));
+            }
+        });
+        refrence.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    i = (int) dataSnapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(newAccount.this,"Registration Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,9 +87,40 @@ public class newAccount extends AppCompatActivity {
                 String lname=last_name.getText().toString();
                 String mail=email.getText().toString();
                 String no=mob_no.getText().toString();
+                String pass=password.getText().toString();
+                if(TextUtils.isEmpty(fname) || TextUtils.isEmpty(lname) || TextUtils.isEmpty(mail) || TextUtils.isEmpty(no) || TextUtils.isEmpty(pass))
+                {
+                    Toast.makeText(newAccount.this,"Empty Credentials",Toast.LENGTH_SHORT).show();
+                }
+                else if (pass.length()<6)
+                {
+                    Toast.makeText(newAccount.this,"Password too short",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    member.setFirst(fname);
+                    member.setLast(lname);
+                    member.setEmail(mail);
+                    member.setNo(no);
+                    member.setPassword(pass);
 
+                    String m1 = patient.getText().toString();
+                    String m2 = ambulance.getText().toString();
+                    String m3 = doctor.getText().toString();
+
+                    if (patient.isChecked()) {
+                        member.setOptions(m1);
+                        refrence.child(String.valueOf(i + 1)).setValue(member);
+                    } else if (ambulance.isChecked()) {
+                        member.setOptions(m2);
+                        refrence.child(String.valueOf(i + 1)).setValue(member);
+                    } else {
+                        member.setOptions(m3);
+                        refrence.child(String.valueOf(i + 1)).setValue(member);
+                    }
+                    Toast.makeText(newAccount.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(newAccount.this,MainActivity.class));
+                }
             }
         });
-
     }
 }
